d8588e2d70c9920be85b466a0b0f356ae3335f14
mohammed.choubby <moch.bff@gmail.com>
2017-12-05 17:42:02 +0000

    adding ngRoute

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,1 +1,11 @@
-angular.module('app', ['shopList','LikedShop']);
+angular.module('app', ['shopList','LikedShop','ngRoute']).config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl : "list.html"
+        })
+        .when("/like", {
+            templateUrl : "liked.html"
+        }).otherwise({
+        redirectTo  : "/"
+        })
+});
